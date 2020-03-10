c2e3d69590da936186b5c0abdd44bdd2937ce980
kateman02 <kateryna.manko@nure.ua>
2020-03-11 00:37:33 +0200

    Init

---
+++ 1.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string name;
+    string surname;
+    int year;
+    
+    cout<<"Enter your name:"<<endl;
+    cin>>name;
+
+    cout<<"Enter your surname:"<<endl;
+    cin>>surname;
+    
+    cout<<"Enter your year:"<<endl;
+    cin>>year;
+
+    cout<<"Your personal information is:"<<name<<" "<<surname<<" "<<year<<endl;
+
+    return 0;
+}
+
+
